7cca794b3023db9efe0e2c9d6c30617982282b05
JomaGsm <jomagsm@gmail.com>
2021-08-05 19:38:55 +0600

    refactoring code

--- lib/data/server_api/models/models/article.dart
+++ lib/data/server_api/models/models/article.dart
@@ -7,48 +7,6 @@ List<Article> articleFromJson(String str) =>
 String articleToJson(List<Article> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
-// class Article {
-//   String title;
-//   String? headerTitle;
-//   String? text;
-//   String? addDate;
-//   String? editDate;
-//   String? lang;
-//   List<OutputImages>? outputImages;
-
-//   Article({
-//     required this.title,
-//     this.headerTitle,
-//     this.text,
-//     this.addDate,
-//     this.editDate,
-//     this.lang,
-//     this.outputImages,
-//   });
-
-//   factory Article.fromJson(Map<String, dynamic> json) => Article(
-//         title: json['title'] as String,
-//         headerTitle: json['headerTitle'] as String?,
-//         text: json['text'] as String?,
-//         addDate: json['addDate'] as String?,
-//         editDate: json['editDate'] as String?,
-//         lang: json['lang'] as String?,
-//         outputImages: (json['outputImages'] as List<dynamic>?)
-//             ?.map((e) => OutputImages.fromJson(e as Map<String, dynamic>))
-//             .toList(),
-//       );
-
-//   Map<String, dynamic> toJson() => {
-//         'title': title,
-//         'headerTitle': headerTitle,
-//         'text': text,
-//         'addDate': addDate,
-//         'editDate': editDate,
-//         'lang': lang,
-//         'outputImages': outputImages?.map((e) => e.toJson()).toList(),
-//       };
-// }
-
 class Article {
   int id;
   List<Images>? images;
